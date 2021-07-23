039231ea8fa7a3678897fa628d41c438573b0d04
Konrad Bauer <konradbauer94@gmail.com>
2021-07-23 18:29:59 +0200

    button text change added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 console.log("Hello world!");
 const themeButton = document.querySelector(".js-themeButton");
-const removeButton = document.querySelector(".js-removeButton")
+const removeButton = document.querySelector(".js-removeButton");
 const body = document.querySelector("body");
-const mainHeader = document.querySelector(".mainHeader")
+const mainHeader = document.querySelector(".mainHeader");
+const themeName = document.querySelector(".themeName");
 
 
 themeButton.addEventListener("click", () => {
     body.classList.toggle("darkTheme")
+    if (body.classList.contains("darkTheme")) {
+        themeName.innerText = "jasny"
+    } else {
+        themeName.innerText = "ciemny"
+    }
 });
 
 removeButton.addEventListener("click", () => {
     mainHeader.remove();
     removeButton.remove();
 });
+
